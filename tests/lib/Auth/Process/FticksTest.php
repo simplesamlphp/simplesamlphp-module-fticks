edfbe4b5fe48d8ff9d5231d4b3d770d415192e10
Guy Halse <guy@tenet.ac.za>
2018-08-31 21:53:31 +0200

    Better unit testing, now we can log to stdout

--- tests/lib/Auth/Process/FticksTest.php
+++ tests/lib/Auth/Process/FticksTest.php
@@ -51,7 +51,6 @@ class sspmod_fticks_Auth_Process_FticksTest extends \PHPUnit_Framework_TestCase
     {
         \SimpleSAML_Configuration::loadFromArray(array(
             'secretsalt' => 'secretsalt',
-            'logging.format' => '%msg',
         ), '[ARRAY]', 'simplesaml');
         /*
         $rm = new ReflectionMethod('SimpleSAML\Logger', 'createLoggingHandler');
@@ -62,33 +61,37 @@ class sspmod_fticks_Auth_Process_FticksTest extends \PHPUnit_Framework_TestCase
 
     public function testMinimal()
     {
-        $config = array('federation' => 'ACME',);
+        $config = array('federation' => 'ACME', 'logdest' => 'stdout');
         $request = self::$_minrequest;
+        $this->expectOutputRegex('/^'.preg_quote('F-TICKS/ACME/1.0#RESULT=OK#AP=https://localhost/sp#RP=https://localhost/idp#CSI=CL','/').'[^#]+#TS=\d+#$/');
         $result = self::processFilter($config, $request);
     }
 
     public function testAsServiceProvider()
     {
-        $config = array('federation' => 'ACME',);
+        $config = array('federation' => 'ACME', 'logdest' => 'stdout',);
         $request = array_merge(self::$_minrequest, self::$_sprequest);
+        $this->expectOutputRegex('/^'.preg_quote('F-TICKS/ACME/1.0#RESULT=OK#AP=https://localhost/saml:sp:IdP#RP=https://localhost/idp#CSI=CL','/').'[^#]+'.preg_quote('#AM=urn:oasis:names:tc:SAML:2.0:ac:classes:unspecified#TS=1000#','/').'$/');
         $result = self::processFilter($config, $request);
     }
 
     public function testSPwithUserId()
     {
-        $config = array('federation' => 'ACME', 'userId' => 'eduPersonPrincipalName');
+        $config = array('federation' => 'ACME', 'logdest' => 'stdout', 'userId' => 'eduPersonPrincipalName');
         $request = array_merge(self::$_minrequest, self::$_sprequest, array(
             'Attributes' => array(
                 'eduPersonPrincipalName' => 'user2@example.net',
             ),
         ));
+        $this->expectOutputRegex('/^'.preg_quote('F-TICKS/ACME/1.0#RESULT=OK#AP=https://localhost/saml:sp:IdP#RP=https://localhost/idp#CSI=CL','/').'[^#]+'.preg_quote('#AM=urn:oasis:names:tc:SAML:2.0:ac:classes:unspecified#PN=e5d066a96d5809a21264e153013c3c793e6574cb77afdfa248ad2cefab9b0451#TS=1000#','/').'$/');
         $result = self::processFilter($config, $request);
     }
 
     public function testAsIdentityProvider()
     {
-        $config = array('federation' => 'ACME',);
+        $config = array('federation' => 'ACME', 'logdest' => 'stdout',);
         $request = array_merge(self::$_minrequest, self::$_idprequest);
+        $this->expectOutputRegex('/^'.preg_quote('F-TICKS/ACME/1.0#RESULT=OK#AP=https://localhost/sp#RP=https://localhost/idp#CSI=CL','/').'[^#]+'.preg_quote('#AM=urn:oasis:names:tc:SAML:2.0:ac:classes:Password#PN=d844a9a0666bb3990e88f72b8f5c20accbcfa46f7b8a7ab38593bfbbab6e9cbc#TS=','/').'\d+#$/');
         $result = self::processFilter($config, $request);
     }
 
@@ -101,6 +104,7 @@ class sspmod_fticks_Auth_Process_FticksTest extends \PHPUnit_Framework_TestCase
             'realm' => 'schacHomeOrganization',
             'algorithm' => 'sha512',
             'exclude' => array('PN'),
+            'logdest' => 'stdout',
         );
         $request = array_merge(self::$_minrequest, self::$_idprequest, array(
             'Attributes' => array(
@@ -108,20 +112,23 @@ class sspmod_fticks_Auth_Process_FticksTest extends \PHPUnit_Framework_TestCase
                 'schacHomeOrganization' => 'example.com',
             ),
         ));
+        $this->expectOutputRegex('/^'.preg_quote('F-TICKS/ACME/1.0#RESULT=OK#AP=https://localhost/sp#RP=https://localhost/idp#CSI=CL','/').'[^#]+'.preg_quote('#AM=urn:oasis:names:tc:SAML:2.0:ac:classes:Password#TS=','/').'\d+#REALM=example.com#$/');
         $result = self::processFilter($config, $request);
     }
 
     public function testFilteringArray()
     {
-        $config = array('federation' => 'ACME', 'exclude' => array('PN', 'AM'));
+        $config = array('federation' => 'ACME', 'logdest' => 'stdout', 'exclude' => array('PN', 'AM'));
         $request = array_merge(self::$_minrequest, self::$_idprequest);
+        $this->expectOutputRegex('/^'.preg_quote('F-TICKS/ACME/1.0#RESULT=OK#AP=https://localhost/sp#RP=https://localhost/idp#CSI=CL','/').'[^#]+#TS=\d+#$/');
         $result = self::processFilter($config, $request);
     }
 
     public function testFilteringString()
     {
-        $config = array('federation' => 'ACME', 'exclude' => 'AM');
+        $config = array('federation' => 'ACME', 'logdest' => 'stdout', 'exclude' => 'AM');
         $request = array_merge(self::$_minrequest, self::$_idprequest);
+        $this->expectOutputRegex('/^'.preg_quote('F-TICKS/ACME/1.0#RESULT=OK#AP=https://localhost/sp#RP=https://localhost/idp#CSI=CL','/').'[^#]+'.preg_quote('#PN=d844a9a0666bb3990e88f72b8f5c20accbcfa46f7b8a7ab38593bfbbab6e9cbc#TS=','/').'\d+#$/');
         $result = self::processFilter($config, $request);
     }
 }
